a18222518fd68fa0b53c2141e2bbe7f20e71477b
Praveen N <npraveen@live.in>
2018-06-20 21:55:14 +0530

    codacy fix

--- javascript/RagGrid.js
+++ javascript/RagGrid.js
@@ -36,10 +36,11 @@ HTMLWidgets.widget({
         return {
 
             renderValue: function(x) {
-                if(document.location.search.indexOf("viewer_pane=1")!=-1 && navigator.platform==="Win32")
+                if(document.location.search.indexOf("viewer_pane=1")!==-1 && navigator.platform==="Win32")
                 {
+                    var url=document.location.href.replace("viewer_pane=1","");
                     el.innerHTML="ag-grid uses CSS transform to position rows in the grid. It looks like your Rstudio Viewer Pane doesn't support this. <br>"
-                    +"Open the <a href='"+document.location.href.replace("viewer_pane=1","")+"'>link</a> in other browser or use the Show in New Window button from the tools."
+                    +`Open the <a href='${url}'>link</a> in other browser or use the Show in New Window button from the tools.`;
                     return;
                 }
                 
